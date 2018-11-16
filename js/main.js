8c47683ba0db1c2ae5a69ad8af39448ed5512d0f
George Park <georgewpark@outlook.com>
2018-11-16 16:13:58 +0000

    set cube color css variable to transcript value if valid

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const recognition = new SpeechRecognition(),
           colorName = document.querySelector('.color-name'),
-          colorCubeFaces = document.querySelectorAll('.cube-face'),
+          colorCube = document.querySelector('.color-cube'),
           heardOutput = document.querySelector('.heard-output'),
           confidenceOutput = document.querySelector('.confidence-output'),
           cssColorNames = ["aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgrey", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "grey", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgrey", "lightgreen", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "transparent", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"];
@@ -24,8 +24,8 @@ window.addEventListener("DOMContentLoaded", () => {
         //Check if transcript is valid color value
         if(cssColorNames.includes(transcript)) {
 
-            //Set color square background color;
-            colorCubeFaces.forEach(face => face.style.backgroundColor = transcript);
+            //Set cube color;
+            colorCube.style.setProperty('--cube-color', transcript);
 
             //Set color name text value
             colorName.textContent = transcript;
